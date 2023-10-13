d66c222e5f71820e2a509abf97ec88eb712d5f8b
marineAqu <102001809+marineAqu@users.noreply.github.com>
2023-10-13 19:03:56 +0900

    first commit

---
+++ src/main/java/com/project/fofo/FofoApplication.java
@@ -0,0 +1,14 @@
+package com.project.fofo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FofoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FofoApplication.class, args);
+	}
+
+	//테스트용 주석 코드
+}
---
+++ src/test/java/com/project/fofo/FofoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.fofo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FofoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
