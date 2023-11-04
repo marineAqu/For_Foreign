3bbfd4925177d84d84bbdddd604f261c1a111f88
marineAqu <102001809+marineAqu@users.noreply.github.com>
2023-11-04 12:04:00 +0900

    [수정] 주석 추가 (2023.11.04 김도연)

--- src/main/java/com/project/fofo/entity/BoardlikeEntity.java
+++ src/main/java/com/project/fofo/entity/BoardlikeEntity.java
@@ -17,6 +17,7 @@ public class BoardlikeEntity {
     @Id // pk 지정
     @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment
     private Long no; //번호
+    //기본키가 필요없으므로 없이 하려고 했으나 jpa 오류로 인해 추가
 
     @Column
     private Long boardNo; //게시글 번호
