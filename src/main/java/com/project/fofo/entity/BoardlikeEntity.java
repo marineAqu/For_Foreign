d2b13ae847facd55ce0db6be2845af23a0071c23
marineAqu <102001809+marineAqu@users.noreply.github.com>
2023-11-04 12:00:57 +0900

    [수정] jpa 오류로 인한 기본키 추가 (2023.11.04 김도연)

--- src/main/java/com/project/fofo/entity/BoardlikeEntity.java
+++ src/main/java/com/project/fofo/entity/BoardlikeEntity.java
@@ -14,6 +14,10 @@ import lombok.Setter;
 @Getter
 @Table(name = "Boardlike")
 public class BoardlikeEntity {
+    @Id // pk 지정
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment
+    private Long no; //번호
+
     @Column
     private Long boardNo; //게시글 번호
 
