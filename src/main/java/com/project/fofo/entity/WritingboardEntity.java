8073a5d04e83344ca80744386daeea9ee2d606c7
marineAqu <102001809+marineAqu@users.noreply.github.com>
2023-11-05 18:34:40 +0900

    [수정] 작문 게시판 관련 기능 추가 (2023.11.05 김도연)

--- src/main/java/com/project/fofo/entity/WritingboardEntity.java
+++ src/main/java/com/project/fofo/entity/WritingboardEntity.java
@@ -30,4 +30,24 @@ public class WritingboardEntity {
     @Column
     private Date creationDate; //작성일
 
+    @ManyToOne
+    @JoinColumn(name = "userNo", referencedColumnName = "no", insertable = false, updatable = false)
+    private MemlistEntity user;
+
+    /**
+     * 함수: toPostWriting
+     * 작성자: 김도연
+     * 설명: 작문 게시글 저장을 위한 함수. 게시글 내용, 날짜를 받고 유저 번호는 임시로 하드코딩함.
+     * */
+    public static WritingboardEntity toPostWriting(String topicContent, Date date) {
+        WritingboardEntity writingboardEntity = new WritingboardEntity();
+
+        writingboardEntity.setUserNo(2L);
+        writingboardEntity.setContent(topicContent);
+        //유저 번호는 일단 하드코딩함
+        //TOD O: 아래 코드 수정 필요(유저 번호 하드코딩)
+        writingboardEntity.setCreationDate(date);
+
+        return writingboardEntity;
+    }
 }
