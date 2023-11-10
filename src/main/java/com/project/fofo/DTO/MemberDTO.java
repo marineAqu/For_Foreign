7d900e771bdc82f0b6639e2a600bc18e9c788b3c
marineAqu <102001809+marineAqu@users.noreply.github.com>
2023-11-10 19:51:18 +0900

    [수정] 랭킹 로직 추가 (2023.11.10 김도연)

--- src/main/java/com/project/fofo/DTO/MemberDTO.java
+++ src/main/java/com/project/fofo/DTO/MemberDTO.java
@@ -5,6 +5,8 @@ package com.project.fofo.DTO;
  * 작성자: 김민정
  **/
 
+import com.project.fofo.entity.MemlistEntity;
+import com.project.fofo.entity.VocalistEntity;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Getter;
@@ -26,11 +28,27 @@ public class MemberDTO {
     @Length(min=5, message = "비밀번호는 8자 이상입니다.")
     private String password;
 
+
+    //userPoint 변수 추가 작성자: 김도연 (랭킹때문에 추가)
+    private Integer userPoint;
+
     @Builder
     public MemberDTO(String userName, String uid, String password) {
         this.userName = userName;
         this.uid = uid;
         this.password = password;
+    }
+
+    /**
+     * 함수: toMemberDTO
+     * 작성자: 김도연
+     * 설명: 랭킹 페이지에서 사용
+     **/
+    public static MemberDTO toMemberDTO(MemlistEntity memlistEntity){
+        MemberDTO memberDTO = new MemberDTO();
+        memberDTO.setUserName(memlistEntity.getUserName());
+        memberDTO.setUserPoint(memlistEntity.getUserPoint());
+        return memberDTO;
     }
 
 }
--- src/main/java/com/project/fofo/repository/MemberRepository.java
+++ src/main/java/com/project/fofo/repository/MemberRepository.java
@@ -8,7 +8,16 @@ package com.project.fofo.repository;
 import com.project.fofo.entity.MemlistEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MemberRepository extends JpaRepository<MemlistEntity, Long> {
     MemlistEntity findByUid(String uid);
     MemlistEntity findByUserName(String name);
+
+    /**
+     * 함수: findByUserPointGreaterThanOrderByUserPointDesc
+     * 작성자: 김도연
+     * 설명: 학습포인트가 0이 아닌 모든 것을 select하고 내림차 정렬
+     **/
+    List<MemlistEntity> findByUserPointGreaterThanOrderByUserPointDesc(Integer userPoint);
 }
