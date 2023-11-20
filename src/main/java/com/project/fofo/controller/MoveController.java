08de5c66482f05e4bc0a1d00ca0325af01e4b820
marineAqu <102001809+marineAqu@users.noreply.github.com>
2023-11-20 09:40:45 +0900

    [수정] +버튼 클릭 시 단어를 단어장에 저장 (2023.11.20 김도연)

--- src/main/java/com/project/fofo/controller/MoveController.java
+++ src/main/java/com/project/fofo/controller/MoveController.java
@@ -1,13 +1,25 @@
 package com.project.fofo.controller;
 
+import com.project.fofo.entity.MemlistEntity;
+import com.project.fofo.service.MemberService;
+import com.project.fofo.service.QuizService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 /**
  * 파일명: MoveController
  * 작성자: 정채빈
  **/
 @Controller
+@RequiredArgsConstructor //김도연 추가
 public class MoveController {
+    private final MemberService memberService; //김도연 추가
+    private final QuizService quizService; //김도연 추가
+
+
     ///페이지 이동
     @GetMapping("nextcamera")
     public String nextcamera(){
@@ -20,5 +32,13 @@ public class MoveController {
     }
 
     @GetMapping("imgtrans")
-    public String imgtransPage() {return "Imgtrans";}
+    public String imgtransPage(Model model) {
+        //imgtransPage 함수 코드 작성: 김도연
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String uid = ((UserDetails) principal).getUsername();
+        MemlistEntity user = memberService.findByMember(uid);
+
+        model.addAttribute("bookLis", quizService.SearchVocaBook(user.getNo().longValue()));
+
+        return "Imgtrans";}
 }
