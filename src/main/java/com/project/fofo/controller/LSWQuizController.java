2b3365691cff790e03d6efdef53bd649f8110593
USER <guswl1424@gmail.com>
2023-11-24 16:08:25 +0900

    [추가, 수정] 듣고쓰기퀴즈 후 종료 페이지 추가, 학습 포인트 저장되도록 수정 (2023.11.24 김현지)

--- src/main/java/com/project/fofo/controller/LSWQuizController.java
+++ src/main/java/com/project/fofo/controller/LSWQuizController.java
@@ -8,6 +8,7 @@ package com.project.fofo.controller;
 import com.project.fofo.DTO.QuizDTO;
 import com.project.fofo.entity.MemlistEntity;
 import com.project.fofo.entity.WordsEntity;
+import com.project.fofo.repository.BoardRepository;
 import com.project.fofo.repository.QuizRepository;
 import com.project.fofo.service.MemberService;
 import com.project.fofo.service.QuizService;
@@ -19,7 +20,9 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 import java.util.*;
@@ -32,6 +35,7 @@ public class LSWQuizController {
     private final QuizRepository quizRepository;
     private final QuizService quizService;
     private final MemberService memberService;
+    private final BoardRepository boardRepository;
 
     @GetMapping("/BookLisLSW")
     public String BookLis(Model model){
@@ -83,6 +87,30 @@ public class LSWQuizController {
         return "LisSenWr";
     }
 
+    @GetMapping("/endOfLSW")
+    public String endOfLSW(Model model, @RequestParam("vocaNo") Long vocaNo){
+        //전체 문제, 맞은 개수
+        List<WordsEntity> wordsEntity = quizRepository.findByVocaNo(vocaNo);
+
+        //단어리스트
+        model.addAttribute("boardList", boardRepository.findByVocaNo(vocaNo));
+
+        //모달
+        model.addAttribute("totNum", wordsEntity.size()); //총 개수
+        model.addAttribute("correctNum", wordsEntity.stream().filter(entity -> String.valueOf(entity.getCheckQuiz()).equals("y")).count()); //맞은 개수
+
+        return "EndOfLSW";
+    }
+
+    @PostMapping("studyPointLSW")
+    public @ResponseBody void studyPointLSW(@RequestParam("correctNum") int correctNum) {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String uid = ((UserDetails) principal).getUsername();
+        MemlistEntity user = memberService.findByMember(uid);
+
+        quizService.givingPoint(user, 20, correctNum);
+
+    }
 
 
 
