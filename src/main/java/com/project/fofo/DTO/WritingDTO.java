1d51ed8088b4bf0041427655ff37f9c5a1b7af08
marineAqu <102001809+marineAqu@users.noreply.github.com>
2024-01-15 19:45:01 +0900

    [수정] 좋아요 개수 보이도록 수정 (2024.01.15 김도연)

--- src/main/java/com/project/fofo/DTO/WritingDTO.java
+++ src/main/java/com/project/fofo/DTO/WritingDTO.java
@@ -29,6 +29,7 @@ public class WritingDTO {
     private String userName; //user에서 userName을 받아옴
     private int heart; //"좋아요"가 actvie인지 아닌지 체크
     private MemlistEntity user; //member테이블에서 조인
+    private int likeCount; //조인해서 가져옴
     private List<BoardlikeEntity> boardlikeEntities; //boardlike 테이블에서 조인 (즉 여러개가 나올 수 있음)
 
     //게시글을 검색하는 DTO
@@ -48,7 +49,9 @@ public class WritingDTO {
         //값이 있는지에 따라 setheart 설정
         //이 안에서 다시 쿼리를 써서 연결하면 오류는 안 나겠지만 그러면 시간 효율이 너무 떨어짐 최대한 boardlikeEntities만으로 해결해야 할 것 같음 => 해결
         writingDTO.setHeart(0);//일단 0으로 설정하고
+        writingDTO.setLikeCount(0);//일단 0으로 설정하고
         for (BoardlikeEntity boardlikeEntity : writingboardEntity.getBoardlikeEntities()) {
+            writingDTO.setLikeCount(writingDTO.getLikeCount()+1); //1 추가
             if (boardlikeEntity.getUserNo().equals(userNo.longValue())) {
                 writingDTO.setHeart(1); //존재하면 1로 바뀜
             }
